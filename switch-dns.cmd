c43b16851efcf428b1148ce2f2fd0863b97da5a9
Ahmadsyar <ahmadsyar@gmail.com>
2025-10-11 09:16:32 +0800

    Update switch-dns.cmd
    more human readable numbering

--- switch-dns.cmd
+++ switch-dns.cmd
@@ -4,26 +4,21 @@ net session >nul 2>&1
 if %errorLevel% neq 0 (
     echo Requesting admin privileges...
     powershell -Command "Start-Process '%~f0' -Verb RunAs"
-    exit /b
+    exit /b 1
 )
 Title "dnsservers switch"
-echo Press 1 to set to cloudflare
+echo Press 1 to set to dhcp
 echo Press 2 to set to google public
-echo Press 3 to set to quad 9
-echo Press 4 to set to adguard
-echo Press 5 to set to tiarapp
-echo Press 6 to reset to next dns
-echo Press 7 to reset to dhcp
-choice /c:1234567 /D:7 /T:10
-if errorlevel == 7 goto seven
-if errorlevel == 6 goto six
-if errorlevel == 5 goto five
-if errorlevel == 4 goto four
-if errorlevel == 3 goto three
-if errorlevel == 2 goto two
-if errorlevel == 1 goto one
+echo Press 3 to set to adguard
+echo Press 4 to set to blissdns
+rem choice /c:1234 /D:1 /T:10 yang lama
+choice /c 1234 /m "Enter your choice: "
+if %errorlevel% == 1 goto dhcp
+if %errorlevel% == 2 goto google
+if %errorlevel% == 3 goto adguard
+if %errorlevel% == 4 goto blissdns
 
-:seven
+:dhcp
 rem set to dhcp
 netsh interface ipv4 set dnsservers "Ethernet" source=dhcp
 netsh interface ipv6 set dnsservers "Ethernet" source=dhcp
@@ -32,57 +27,7 @@ netsh interface ipv6 set dnsservers "Wi-Fi" source=dhcp
 ipconfig -flushdns
 goto end
 
-:six
-rem set to next dns
-netsh interface ipv4 set dnsservers "Ethernet" static 45.90.28.225
-netsh interface ipv4 add dnsservers "Ethernet" 45.90.30.225 index=2
-netsh interface ipv6 set dnsservers "Ethernet" static 2a07:a8c0::ec:a14c
-netsh interface ipv6 add dnsservers "Ethernet" 2a07:a8c1::ec:a14c index=2
-netsh interface ipv4 set dnsservers "Wi-Fi" static 45.90.28.225
-netsh interface ipv4 add dnsservers "Wi-Fi" 45.90.30.225 index=2
-netsh interface ipv6 set dnsservers "Wi-Fi" static 2a07:a8c0::ec:a14c
-netsh interface ipv6 add dnsservers "Wi-Fi" 2a07:a8c1::ec:a14c index=2
-ipconfig -flushdns
-goto end
-
-:five
-rem set to tiarap
-netsh interface ipv4 set dnsservers "Ethernet" static 174.138.21.128
-netsh interface ipv4 add dnsservers "Ethernet" 188.166.206.224 index=2
-netsh interface ipv6 set dnsservers "Ethernet" static 2400:6180:0:d0::5f6e:4001
-netsh interface ipv4 set dnsservers "Wi-Fi" static 174.138.21.128
-netsh interface ipv4 add dnsservers "Wi-Fi" 188.166.206.224 index=2
-netsh interface ipv6 set dnsservers "Wi-Fi" static 2400:6180:0:d0::5f6e:4001
-ipconfig -flushdns
-goto end
-
-:four
-rem set to adguard
-netsh interface ipv4 set dnsservers "Ethernet" static 94.140.14.14
-netsh interface ipv4 add dnsservers "Ethernet" 94.140.15.15 index=2
-netsh interface ipv6 set dnsservers "Ethernet" static 2a10:50c0::ad1:ff
-netsh interface ipv6 add dnsservers "Ethernet" 2a10:50c0::ad2:ff index=2
-netsh interface ipv4 set dnsservers "Wi-Fi" static 94.140.14.14
-netsh interface ipv4 add dnsservers "Wi-Fi" 94.140.15.15 index=2
-netsh interface ipv6 set dnsservers "Wi-Fi" static 2a10:50c0::ad1:ff
-netsh interface ipv6 add dnsservers "Wi-Fi" 2a10:50c0::ad2:ff index=2
-ipconfig -flushdns
-goto end
-
-:three
-rem set to quad 9
-netsh interface ipv4 set dnsservers "Ethernet" static 9.9.9.9
-netsh interface ipv4 add dnsservers "Ethernet" 149.112.112.112 index=2
-netsh interface ipv6 set dnsservers "Ethernet" static 2620:fe::fe
-netsh interface ipv6 add dnsservers "Ethernet" 2620:fe::9 index=2
-netsh interface ipv4 set dnsservers "Wi-Fi" static 9.9.9.9
-netsh interface ipv4 add dnsservers "Wi-Fi" 149.112.112.112 index=2
-netsh interface ipv6 set dnsservers "Wi-Fi" static 2620:fe::fe
-netsh interface ipv6 add dnsservers "Wi-Fi" 2620:fe::9 index=2
-ipconfig -flushdns
-goto end
-
-:two
+:google
 rem set to google public
 netsh interface ipv4 set dnsservers "Ethernet" static 8.8.8.8
 netsh interface ipv4 add dnsservers "Ethernet" 8.8.4.4 index=2
@@ -95,16 +40,25 @@ netsh interface ipv6 add dnsservers "Wi-Fi" 2001:4860:4860::8844 index=2
 ipconfig -flushdns
 goto end
 
-:one
-rem set to cloudflare
-netsh interface ipv4 set dnsservers "Ethernet" static 1.1.1.1
-netsh interface ipv4 add dnsservers "Ethernet" 1.0.0.1 index=2
-netsh interface ipv6 set dnsservers "Ethernet" static 2606:4700:4700::1111
-netsh interface ipv6 add dnsservers "Ethernet" 2606:4700:4700::1001 index=2
-netsh interface ipv4 set dnsservers "Wi-Fi" static 1.1.1.1
-netsh interface ipv4 add dnsservers "Wi-Fi" 1.0.0.1 index=2
-netsh interface ipv6 set dnsservers "Wi-Fi" static 2606:4700:4700::1111
-netsh interface ipv6 add dnsservers "Wi-Fi" 2606:4700:4700::1001 index=2
+:adguard
+rem set to adguard
+netsh interface ipv4 set dnsservers "Ethernet" static 94.140.14.14
+netsh interface ipv4 add dnsservers "Ethernet" 94.140.15.15 index=2
+netsh interface ipv6 set dnsservers "Ethernet" static 2a10:50c0::ad1:ff
+netsh interface ipv6 add dnsservers "Ethernet" 2a10:50c0::ad2:ff index=2
+netsh interface ipv4 set dnsservers "Wi-Fi" static 94.140.14.14
+netsh interface ipv4 add dnsservers "Wi-Fi" 94.140.15.15 index=2
+netsh interface ipv6 set dnsservers "Wi-Fi" static 2a10:50c0::ad1:ff
+netsh interface ipv6 add dnsservers "Wi-Fi" 2a10:50c0::ad2:ff index=2
+ipconfig -flushdns
+goto end
+
+:blissdns
+rem set to blissdns
+netsh interface ipv4 set dnsservers "Ethernet" static 173.255.201.254
+netsh interface ipv6 set dnsservers "Ethernet" static 2600:3c00::f03c:93ff:feca:d2be
+netsh interface ipv4 set dnsservers "Wi-Fi" static 173.255.201.254
+netsh interface ipv6 set dnsservers "Wi-Fi" static 2600:3c00::f03c:93ff:feca:d2be
 ipconfig -flushdns
 goto end
 
